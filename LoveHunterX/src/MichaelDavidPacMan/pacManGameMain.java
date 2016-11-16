b821aac2b42beb7a290d23c6fe5ab284ab376e75
Student 7 <Student 7@10.8.33.168>
2016-11-16 12:34:22 -0500

    .

---
+++ LoveHunterX/src/MichaelDavidPacMan/pacManGameMain.java
@@ -0,0 +1,22 @@
+package MichaelDavidPacMan;
+
+	import java.util.Scanner;
+
+public class pacManGameMain {
+	
+//things we need:
+//	a default grid
+//	a pacman
+//	dogs
+//	pieces of bones
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void generateGrid(){
+		
+	}
+}
