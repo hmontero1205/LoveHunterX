5d9d5aac3b36cdc7d0f4cd6df8e466cad9d2bd26
Student 7 <Student 7@10.8.33.168>
2016-11-17 12:33:45 -0500

    randomGens

--- LoveHunterX/src/MichaelDavidPacMan/CellRoom.java
+++ LoveHunterX/src/MichaelDavidPacMan/CellRoom.java
@@ -3,18 +3,22 @@ package MichaelDavidPacMan;
 public class CellRoom {
 	private boolean accessible;
 	private boolean boneFrag;
-	
+
 	private static boolean genCondition(int desireable, int total){
 		//random generate a true/false statement based on parameter.
 		//ex: i want a 1/4 chance to get a true.
 		boolean[] test = new boolean[total];
 		for(int i = 0; i<desireable; i++){
-			//test.x
+			test[i]=true;
 		}
-		return true;
+		int rand = (int)Math.random() * total;
+		return test[rand];
 	}
+
 	public CellRoom(){
-		
+		accessible = genCondition(8,10);
+		if (accessible){
+			boneFrag = genCondition(1,2);
+		}
 	}
-	
 }
