a59b7f8bf081fc31533cab17e990a85a3657850f
dquinde9306 <dquinde9306@bths.edu>
2016-11-19 10:24:58 -0500

    new stuff

--- LoveHunterX/src/ExplorerMain/Bejeweled.java
+++ LoveHunterX/src/ExplorerMain/Bejeweled.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class Bejeweled {
 	public static String[][]arr2d;
 	public static Scanner in;
+	public static String[] symbol;
 	public static void main(String args[]){
-
-		//setGame();
+		symbol = new String[]{"A","B","C"};
+		
+		setGame();
 		in = new Scanner(System.in);
-		//ask();
-		String[][] test = {{"a","b","c","d"},{"a","e","f","g"},{"a","j","h","i"},{"a","l","m","n"}};
-		printPic(test);
+		ask();
+		//String[][] test = {{"a","b","c","d"},{"a","e","f","g"},{"a","j","h","i"},{"a","l","m","n"}};
+		//printPic(test);
 		//System.out.println("swapping");
 		//swap(test,);
 		// wdf
-		checkCol(test,0);
+		//checkCol(test,0);
 
 	}
 	public static void setGame(){
 		arr2d = new String[2][3];
-		String[] symbol = {"A","B","C"};
-
 		for(int row= 0; row < arr2d.length;row++){
 			for(int col = 0; col < arr2d[row].length;col++){
 				double rand = Math.random();
@@ -72,15 +72,18 @@ public class Bejeweled {
 	}
 
 	public static void ask(){
-		System.out.println("which row?");
-		int row =  Integer.parseInt(in.nextLine());
-		System.out.println("which column?");
-		int col = Integer.parseInt(in.nextLine());
-		System.out.println("which direction?");
-		String dir = in.nextLine();
-		System.out.println(row+ " "+ col + " " +dir);
-		swap(arr2d, row,col,dir);
-
+		int a  = 0;
+		while(a  <2){
+			System.out.println("which row?");
+			int row =  Integer.parseInt(in.nextLine());
+			System.out.println("which column?");
+			int col = Integer.parseInt(in.nextLine());
+			System.out.println("which direction?");
+			String dir = in.nextLine();
+			System.out.println(row+ " "+ col + " " +dir);
+			swap(arr2d, row,col,dir);
+			a++;
+		}
 
 	}
 
@@ -143,7 +146,17 @@ public class Bejeweled {
 
 	}
 
-
+	public static void replaceBlank(String[][] grid){
+		for(int row= 0; row < grid.length;row++){
+			for(int col = 0; col < grid[row].length;col++){
+				if(grid[row][col].equals(" ")){
+					double rand = Math.random();
+					int roll = (int) (symbol.length * rand);
+					grid[row][col] = symbol[roll];
+				}
+			}
+		}
+	}
 }
 
 
