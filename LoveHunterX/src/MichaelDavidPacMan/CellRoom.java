303cded13e5a491004c456e606f7b99b4aec223e
micha <micha@Michael>
2016-11-20 17:07:00 -0500

    .

--- LoveHunterX/src/MichaelDavidPacMan/CellRoom.java
+++ LoveHunterX/src/MichaelDavidPacMan/CellRoom.java
@@ -3,7 +3,7 @@ package MichaelDavidPacMan;
 public class CellRoom {
 	private boolean accessible;
 	private boolean boneFrag;
-
+	
 	private static boolean genCondition(int desireable, int total){
 		//random generate a true/false statement based on parameter.
 		//ex: i want a 1/4 chance to get a true.
@@ -11,15 +11,24 @@ public class CellRoom {
 		for(int i = 0; i<desireable; i++){
 			test[i]=true;
 		}
-		int rand = (int)Math.random() * total;
+		int rand = (int)(Math.random() * total);
 		return test[rand];
 	}
 
 	public CellRoom(){
-		accessible = genCondition(8,10);
+		//chance room is accessible 	
+		this.accessible = genCondition(7,10);
 		if (accessible){
-			boneFrag = genCondition(1,2);
-			//x
+			//if accessible, probability of getting a bone frag.
+			this.boneFrag = genCondition(2,3);
 		}
+	}
+	
+	public boolean getAccessible(){
+		return accessible;
+	}
+	
+	public boolean getBFrag(){
+		return boneFrag;
 	}
 }
